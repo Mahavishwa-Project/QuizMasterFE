787b3313cdf16073acfffa5b2056f6cd5efdbf18
Ekya1411 <ekyamuhammad@gmail.com>
2025-11-24 14:25:03 +0700

    fix: vercel error with protexted Route

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -2,11 +2,19 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import AuthCallback from "@/app/router/auth/AuthCallback";
 import { LoginPage } from "@/pages/user/LoginPage";
 import { Dashboard } from "@/pages/user/Dashboard";
+import { ProtectedRoute } from "@/app/router/ProtectedRoute";
 
 const router = createBrowserRouter([
     { path: "/", element: <LoginPage /> },
     { path: "/auth/callback", element: <AuthCallback /> },
-    { path: "/dashboard", element: <Dashboard /> }
+    { 
+        path: "/dashboard", 
+        element: (
+            <ProtectedRoute>
+                <Dashboard />
+            </ProtectedRoute>
+        ) 
+    }
 ]);
 
 export const AppRouter = () => <RouterProvider router={router} />;
